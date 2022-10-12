d932a05c7acdf96d0df2b3be498c63b9a946a21c
Maxim Bogdanov <mmbogdanov@outlook.com>
2022-10-12 19:26:42 +0300

    perf: change the library to work with the bot

--- adminpanel/management/commands/runbot.py
+++ adminpanel/management/commands/runbot.py
@@ -1,8 +1,7 @@
 from django.core.management import BaseCommand
-from aiogram import executor
 from telebot import bot
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        executor.start_polling(bot.dp, skip_updates=False)
+        bot.run()
--- telebot/bot.py
+++ telebot/bot.py
@@ -1,19 +1,51 @@
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, types
+from telegram import Bot, Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram.utils.request import Request
 from dotenv import load_dotenv
+from adminpanel.models import Profile, Message
 from setting.settings import BASE_DIR
-
 
 logging.basicConfig(level=logging.INFO)
 
 load_dotenv(BASE_DIR / 'config.env')
 
-bot = Bot(token=os.getenv('TG_TOKEN'))
-dp = Dispatcher(bot)
+
+def do_echo(update: Update, context: CallbackContext):
+    # get or add a profile to the DB
+    p, _ = Profile.objects.get_or_create(
+        external_id=update.message.chat_id,
+        defaults={
+            'name': update.message.from_user.full_name
+        }
+    )
+
+    # add a message to the DB
+    m = Message(
+        profile=p,
+        text=update.message.text
+    )
+    m.save()
+
+    update.message.reply_text(update.message.text)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+def run():
+    request = Request(
+        connect_timeout=0.5,
+        read_timeout=1.0
+    )
+    bot = Bot(
+        request=request,
+        token=os.getenv('TG_TOKEN')
+    )
+    updater = Updater(
+        bot=bot,
+        use_context=True
+    )
+
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, do_echo))
+    updater.start_polling()
+    updater.idle()
